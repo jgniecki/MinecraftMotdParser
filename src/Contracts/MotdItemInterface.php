961c925d7589731312f840ef33a09b8e95462e7b
Jakub Gniecki <kubuspl@onet.eu>
2024-06-24 15:46:46 +0200

    set returns type for setter

--- src/Contracts/MotdItemInterface.php
+++ src/Contracts/MotdItemInterface.php
@@ -5,20 +5,20 @@ namespace DevLancer\MinecraftMotdParser\Contracts;
 interface MotdItemInterface
 {
     public function getText(): ?string;
-    public function setText(string $text);
+    public function setText(string $text): void;
     public function getColor(): ?string;
-    public function setColor(string $color);
+    public function setColor(string $color): void;
     public function isObfuscated(): bool;
-    public function setObfuscated(bool $obfuscated);
+    public function setObfuscated(bool $obfuscated): void;
     public function isBold(): bool;
-    public function setBold(bool $bold);
+    public function setBold(bool $bold): void;
     public function isUnderlined(): bool;
-    public function setUnderlined(bool $underlined);
+    public function setUnderlined(bool $underlined): void;
     public function isStrikethrough(): bool;
-    public function setStrikethrough(bool $strikethrough);
+    public function setStrikethrough(bool $strikethrough): void;
     public function isItalic(): bool;
-    public function setItalic(bool $italic);
+    public function setItalic(bool $italic): void;
     public function isReset(): bool;
-    public function setReset(bool $reset);
+    public function setReset(bool $reset): void;
 
 }
